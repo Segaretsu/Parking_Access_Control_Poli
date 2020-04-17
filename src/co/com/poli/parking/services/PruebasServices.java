8e3de75f23ec92fd2cb9aaed5b6a71926e0df6db
Jhon Sebastian <Sebastamriel@gmail.com>
2020-04-16 21:05:21 -0500

    Web Services Config is Working - 16/04/2020 09:05 pm

---
+++ src/co/com/poli/parking/services/PruebasServices.java
@@ -0,0 +1,36 @@
+package co.com.poli.parking.services;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+@Path("pruebas")
+public class PruebasServices {
+
+	
+	@GET
+	@Path("server/estatus")
+	@Produces("text/plain")
+	public String getEstadoWebServices() {
+		/**
+		 * URL Ejemplo: http://localhost:8080/Parking_Access_Control_Poli/Parking-back/pruebas/server/estatus
+		 */
+		return "El servidor se encuentra melo";
+	}
+	
+	@GET
+	@Path("nombreEnviado/{username}")
+    @Produces("text/plain")
+    public String getNombreEjemplo(@PathParam("username") String nombre) {
+		/**
+		 * URL Ejemplo: http://localhost:8080/Parking_Access_Control_Poli/Parking-back/pruebas/nombreEnviado/Juan Perez
+		 */
+        return nombre;
+    }
+	
+	
+	/**
+	 * Para aprender un poco más de los métodos de los web services:
+	 * https://docs.oracle.com/javaee/6/tutorial/doc/gilik.html
+	 */
+}
--- src/co/com/poli/parking/services/WebServicesConfig.java
+++ src/co/com/poli/parking/services/WebServicesConfig.java
@@ -1,5 +1,18 @@
 package co.com.poli.parking.services;
+import java.util.Set;
+import javax.ws.rs.core.Application;
 
-public class WebServicesConfig {
-
+@javax.ws.rs.ApplicationPath("Parking-back")
+public class WebServicesConfig  extends Application{
+	
+	@Override
+	public Set<Class<?>> getClasses(){
+		Set<Class<?>> resources = new java.util.HashSet<>();
+		addRestResourceClasses(resources);
+		return resources;
+	}
+	
+	public void addRestResourceClasses(Set<Class<?>> resources) {
+		resources.add(co.com.poli.parking.services.PruebasServices.class);
+	}
 }
